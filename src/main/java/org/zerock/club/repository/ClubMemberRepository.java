54e0bb5c02164b64eaa4e79cafa06a8644a57a9d
quedevel <kdh90817@naver.com>
2021-02-03 22:44:46 +0900

    Security 작업중

---
+++ src/main/java/org/zerock/club/repository/ClubMemberRepository.java
@@ -0,0 +1,8 @@
+package org.zerock.club.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.zerock.club.entity.ClubMember;
+
+public interface ClubMemberRepository extends JpaRepository<ClubMember, String> {
+
+}
